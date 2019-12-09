82b2a0d3b46d062f6e76ccf9c545226f0ac03775
Se7en <455732253@qq.com>
2019-12-09 18:28:52 +0800

    Update Fool-WPScan.sh

--- Fool-WPScan.sh
+++ Fool-WPScan.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 # author:Se7en
 #使用方法【. Fool-WPScan.sh】
+
 read -p "请输入要检测的网址：" url
 while [[ $str != 'exit' ]]
 do
-    echo "+---------------------------------------------------------------+ "
-    echo "| Talk is cheap , Show me the code.       /\                    | "
-    echo "| ________  __          __               (@@) /)                | "   
-    echo "|/\_____  \/\ \      __/\ \__           //  |/                  | "   
-    echo "|\/___//'/'\ \ \____/\_\ \ ,_\          //H4ck1ng               | "  
-    echo "|    //'/'  \ \ '__'\/\ \ \ \/          //F0r                   | "
-    echo "|   //'/'    \ \ \_\ \ \ \ \ \_         //Fun!                  | "
-    echo  -e "|  /\_/'      \ \_,__/\ \_\ \__\        [\033[5;31mFool-WPScan.sh\033[0m]        | " 
-    echo "|  \//'        \/___/  \/_/\/__/        HTTPS://WWW.SE7ENSEC.CN | "
-    echo "+---------------------------------------------------------------+ "
-    echo "枚举账户：1"
-    echo "爆破密码：2"
-    echo "枚举存在漏洞插件：3"
-    echo "枚举存在漏洞主题：4"
-    echo "TimThumbs文件漏洞：5"
-    echo "退出：exit"
-    read -p "请输入选项：" str
-    case $str in
+	echo "+---------------------------------------------------------------+ "
+	echo "| Talk is cheap , Show me the code.       /\                    | "
+	echo "| ________  __          __               (@@) /)                | "   
+	echo "|/\_____  \/\ \      __/\ \__           //  |/                  | "   
+	echo "|\/___//'/'\ \ \____/\_\ \ ,_\          //H4ck1ng               | "  
+	echo "|    //'/'  \ \ '__'\/\ \ \ \/          //F0r                   | "
+	echo "|   //'/'    \ \ \_\ \ \ \ \ \_         //Fun!                  | "
+	echo  -e "|  /\_/'      \ \_,__/\ \_\ \__\        [\033[5;31mFool-WPScan.sh\033[0m]        | " 
+	echo "|  \//'        \/___/  \/_/\/__/        HTTPS://WWW.SE7ENSEC.CN | "
+	echo "+---------------------------------------------------------------+ "
+	echo "枚举账户：1"
+	echo "爆破密码：2"
+	echo "枚举存在漏洞插件：3"
+	echo "枚举存在漏洞主题：4"
+	echo "TimThumbs文件漏洞：5"
+	echo "退出：exit"
+	read -p "请输入选项：" str
+	case $str in
         1)
             wpscan --url $url --enumerate u --rua
             ;;
@@ -38,8 +39,8 @@ do
             ;;
         5)
             wpscan --url $url --enumerate tt --rua
-            ;;
-    esac
+			;;
+	esac
 done
 unset url
 unset str
